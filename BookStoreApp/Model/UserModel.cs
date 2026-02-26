83767040f665fb176b666d4e5ebfb599f4dc89ae
manju <manjunathdesai861@gmail.com>
2026-02-26 21:56:57 +0530

    added user

---
+++ BookStoreApp/Model/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreApp.Model
+{
+    public class UserModel
+    {
+        public int UsrID { get; set; }
+        public string userName { get; set; }
+    }
+}
